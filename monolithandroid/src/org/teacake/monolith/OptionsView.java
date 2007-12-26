32724a8e2400738e7ec2e33d35d0117994064711
tasos.kleisas <tasos.kleisas@88e73acb-cf41-0410-8ef7-4539a504c32e>
2007-12-26 15:38:08 +0000

    made some changes to support the new default emulator skin HVGA-P

---
+++ monolithandroid/src/org/teacake/monolith/OptionsView.java
@@ -0,0 +1,33 @@
+package org.teacake.monolith;
+
+import android.view.View;
+import android.content.Context;
+import android.content.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.view.KeyEvent;
+import android.util.AttributeSet;
+import android.os.Bundle;
+
+import java.util.Map;
+public class OptionsView extends View {
+	public OptionsView(Context context)
+	{
+        super(context);
+
+        
+        
+
+	}
+	android.widget.Button buttonIncreaseLevel;
+	android.widget.Button buttonDecreaseLevel;
+	android.widget.TextView textViewLevel;
+	android.widget.Button buttonGameModeNext;
+	android.widget.Button buttonGameModePrevious;
+	android.widget.TextView textViewGameMode;
+	
+	
+	
+}
