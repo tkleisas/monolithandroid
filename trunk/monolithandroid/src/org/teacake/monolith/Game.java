9f65d3a17f2d2d366037c216a471a40012b79b47
tasos.kleisas <tasos.kleisas@88e73acb-cf41-0410-8ef7-4539a504c32e>
2008-01-02 17:13:39 +0000

    Version with user selectable game modes and intro screen

---
+++ trunk/monolithandroid/src/org/teacake/monolith/Game.java
@@ -0,0 +1,38 @@
+package org.teacake.monolith;
+
+public interface Game
+{
+	public static final int STATUS_STARTUP=0;
+	public static final int STATUS_PLAYING=1;
+	public static final int STATUS_PAUSE=2;
+	public static final int STATUS_GAME_OVER=3;
+	public static final int STATUS_EVOLVING=4;
+	public void clearGrid();
+	public void initGame(int theStartingLevel);
+	public void setStatus(int status);
+	public int getStatus();
+	public int getGridValue(int x, int y);
+	public void setGridValue(int x, int y, int value);
+	public void gameLoop();
+	public void moveBlockLeft();
+	public void moveBlockRight();
+	public boolean moveBlockDown();
+	public void clearCompleteLines();
+	public void flagCompletedLines();
+	public int getClearedLineCount();
+	
+	public void rotateCurrentBlockClockwise();
+	public void setScore(int score);
+	public int getScore();
+	public void setLevel(int level);
+	public int getLevel();
+	public void setLines(int lines);
+	public int getLines();
+	public void setTimerEnabled(boolean flag);
+	public int getEnergy();
+	public void setEnergy(int energy);
+	public boolean isTimerEnabled();
+	public Block getCurrentBlock();
+	public Block getNextBlock();
+	public int getTimer();
+}
