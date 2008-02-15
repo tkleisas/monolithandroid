434f1dfae581cd95783b22bbdb760df2fb852a65
Tasos.Kleisas <Tasos.Kleisas@88e73acb-cf41-0410-8ef7-4539a504c32e>
2008-02-15 11:09:07 +0000

    First steps to support m5-rc14b SDK

---
+++ trunk/monolithandroid/src/org/teacake/monolith/GameSurfaceView.java
@@ -0,0 +1,16 @@
+package org.teacake.monolith;
+import android.content.Context;
+import android.graphics.OpenGLContext;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameSurfaceView extends SurfaceView
+{
+	public GameSurfaceView(Context context)
+	{
+        super(context);
+        //init();
+	}
+	
+}
