8e6262edffc6e9ddf0c36a611f5ea697423a4aab
Tasos.Kleisas <Tasos.Kleisas@88e73acb-cf41-0410-8ef7-4539a504c32e>
2008-05-25 18:20:47 +0000

    added Starfield. A class for drawing stars.

--- trunk/monolithandroid/src/org/teacake/util/FixedPointFloat.java
+++ trunk/monolithandroid/src/org/teacake/util/FixedPointFloat.java
@@ -4,26 +4,15 @@ public class FixedPointFloat {
 	
 	public static int floatToFixedPoint(float number)
 	{
-		java.math.BigDecimal bd = new java.math.BigDecimal(number);
-		
-			return Float.floatToIntBits(number)*256;
+
+                return (int)(number * 65536F);
 	}
+	
 	public static float fixedPointToFloat(int number)
 	{
-		float retval =0.0f;
-		retval = retval - (number&(1<<31));
-		int intpart = (number>>16)&0xffff;
-		int fractpart = number&0xffff;
-		for (int i=0;i<15;i++)
-		{
-			retval+=(1<<i)&intpart;
-		}
-		for (int i=0;i<16;i++)
-		{
-			retval+=(fractpart>>(15-i))*(1.0/(2>>i));
-		}
-		return retval;
-		
-	}
+
+            return ((float)number)/65536.0f;
+    }
+
 	
 }
